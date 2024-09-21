7594f06c49989a78eff4593667986585ebc0bbec
Tban <horlaryinka0@gmail.com>
2024-09-21 08:02:03 +0100

    pay

--- src/components/UI/PaymentSuccess/index.tsx
+++ src/components/UI/PaymentSuccess/index.tsx
@@ -11,6 +11,7 @@ const PaymentSuccess = () => {
   const [amount, setAmount] = useState<number | null>(null);
   const [loading, setLoading] = useState<boolean>(true);
   const [error, setError] = useState<string | null>(null);
+  const [hasSentData, setHasSentData] = useState<boolean>(false);
 
  
   useEffect(() => {
@@ -40,10 +41,10 @@ const PaymentSuccess = () => {
             formPayload.append('Email', formData.email);
             formPayload.append('Phone', formData.phone);
             formPayload.append('Amount', parsedAmount?.toString() || '');
-
+            // https://script.google.com/macros/s/AKfycbxLK4MT95n8ff16lzEJLl5524Yt1IqeJj2g367A3qOS7WdyM7YdekRZbDtgU2KpOIzaXA/exec
             try {
               const response = await fetch(
-                'https://script.google.com/macros/s/AKfycbxLK4MT95n8ff16lzEJLl5524Yt1IqeJj2g367A3qOS7WdyM7YdekRZbDtgU2KpOIzaXA/exec  ',
+                'https://script.google.com/macros/s/AKfycbxqQlq8Sa4tHoJbBV_z7wzQgXZ3LvYa5lR7vbuv_r9TB9UtEg_iSJP3ZTgmygK4aB5S1g/exec',
                 {
                   method: 'POST',
                   body: formPayload,
@@ -79,6 +80,11 @@ const PaymentSuccess = () => {
   }, []);
 
 
+ 
+  
+  
+  
+  
 
   return(
     <Container>
